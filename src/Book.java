953ea09207a069f8911ab9931cb4b3ea656eae99
fungamer153 <getrext10@gmail.com>
2024-03-10 22:09:37 +0000

    Fixes

--- src/Book.java
+++ src/Book.java
@@ -1,16 +1,40 @@
+/**
+ * Book class represents a created book object that contains mutator and getter
+ * methods allowing the book object to be modified and have its contents
+ * changed.
+ *
+ * @author Maksim Strelkov
+ * - no copyright
+ * @version 1.0
+ * @since 2024-03-10
+ */
+
 public class Book {
+    /** String array for multi authored books */
     private String[] authors = {};
+
+    /** Author names for books written by single author */
     private String authorFamilyName;
     private String authorRestOfName;
+
+    /** Information pertaining the created book object */
     private String title;
     private String publisher;
     private int numPages;
     private int year;
     private final int bookID;
+
+    /** Unique id that is set for each book, begins from 1004825 */
     private static int id = 1004825;
     private final Genre genre;
 
-
+    /**
+     * Basic Book class constructor for books with a single author.
+     * @param authorFN Author's family name.
+     * @param authorRN Rest of author's name.
+     * @param title What the book is called.
+     * @param genre What genre the book belongs in.
+     */
     public Book(String authorFN, String authorRN, String title, Genre genre) {
         this.authorFamilyName = authorFN;
         this.authorRestOfName = authorRN;
@@ -19,6 +43,17 @@ public class Book {
         this.bookID = id++;
     }
 
+    /**
+     * Book class constructor with more in-depth information for books with a
+     * single author.
+     * @param authorFN Author's family name.
+     * @param authorRN Rest of author's name.
+     * @param title What the book is called.
+     * @param genre What genre the book belongs in.
+     * @param year What year the book was released in.
+     * @param publisher The group / person that released it.
+     * @param numPages How many pages it contains.
+     */
     public Book(String authorFN, String authorRN, String title, Genre genre, int year, String publisher, int numPages) {
         this.authorFamilyName = authorFN;
         this.authorRestOfName = authorRN;
@@ -30,6 +65,17 @@ public class Book {
         this.bookID = id++;
     }
 
+    /**
+     * Book class constructor for books with multiple authors, with in-depth
+     * detail about the book.
+     * @param authors Array containing strings of author's family name
+     *                & rest of their name
+     * @param title What the book is called.
+     * @param genre What genre the book belongs in.
+     * @param year What year the book was released in.
+     * @param publisher The group / person that released it.
+     * @param numPages How many pages it contains.
+     */
     public Book(String[] authors, String title, Genre genre, int year, String publisher, int numPages) {
         this.authors = authors;
         this.title = title;
@@ -40,6 +86,11 @@ public class Book {
         this.bookID = id++;
     }
 
+    /**
+     * Returns the author's family name, if the book has multiple authors
+     * then returns the first author's family name.
+     * @return String containing the author's family name.
+     */
     public String getAuthorFamilyNames() {
         if (this.authorFamilyName != null) {
             return this.authorFamilyName;
@@ -48,33 +99,68 @@ public class Book {
         }
     }
 
+    /**
+     * Gets the rest of author's name.
+     * @return String containing the rest of author's name.
+     */
     public String getAuthorRestOfName() {
         return this.authorRestOfName;
     }
 
+    /**
+     * Gets the genre that the book is in.
+     * @return A genre enum of the genre that book is in.
+     */
     public Genre getGenre() {
         return this.genre;
     }
+
+    /**
+     * Gets the title of the book.
+     * @return String containing title of the book.
+     */
     public String getTitle() {
         return this.title;
     }
 
+    /**
+     * Gets the unique book ID from the book.
+     * @return Integer of the ID.
+     */
     public int getBookID() {
         return this.bookID;
     }
 
+    /**
+     * Gets how many pages the book contains.
+     * @return Integer of how many pages are in the book.
+     */
     public int getNumPages() {
         return this.numPages;
     }
 
+    /**
+     * Gets the person / group that published the book.
+     * @return String that contains name of the publisher.
+     */
     public String getPublisher() {
         return this.publisher;
     }
 
+    /**
+     * Gets the year the book was released in.
+     * @return Integer of the year the book was released in.
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * Outputs a citation depending on what genre the book is set in,
+     * including important book information like the publisher, year and
+     * how many pages are inside.
+     * @return Formatted string of the books' information.
+     */
     public String getCitation() {
         if (this.authorFamilyName == null) {
             String finalOutput = "";
@@ -100,7 +186,7 @@ public class Book {
                         + ", " + this.authorRestOfName
                         + ". " + this.title
                         + ".";
-            } else if (this.genre == Genre.NON_FICTION) {
+            } else {
                 return this.authorFamilyName
                         + ", " + this.authorRestOfName
                         + ". " + this.title
@@ -110,14 +196,20 @@ public class Book {
                         + "pp.";
             }
         }
-
-        return "";
     }
 
+    /**
+     * Sets the author's family name to the given string.
+     * @param authorFamilyName String of the new family name.
+     */
     public void setAuthorFamilyName(String authorFamilyName) {
         this.authorFamilyName = authorFamilyName;
     }
 
+    /**
+     * Adds an authors name to the list if the book has multiple authors.
+     * @param name Name of the new added author.
+     */
     public void setAuthorFamilyNames(String name) {
         if (this.authors.length == 0) {
             this.authors = new String[]{name};
@@ -133,14 +225,26 @@ public class Book {
         }
     }
 
+    /**
+     * Set the rest of author's name for the book to given string.
+     * @param authorRestOfName The new rest of authors name.
+     */
     public void setAuthorRestOfName(String authorRestOfName) {
         this.authorRestOfName = authorRestOfName;
     }
 
+    /**
+     * Set the book title to the new given string
+     * @param title The new title for the book.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Format all attributes of the class into a readable string
+     * @return String including all attributes of the book object.
+     */
     @Override
     public String toString() {
         return this.authorFamilyName
@@ -150,6 +254,10 @@ public class Book {
                 + this.bookID;
     }
 
+    /**
+     * Formats a shorter version of toString.
+     * @return Less attribute information pertaining to the book.
+     */
     public String getShortString() {
         if (this.authorFamilyName != null) {
             return this.authorFamilyName
